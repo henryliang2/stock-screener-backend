d23fb0ae71d0962b722448fde46b82006169c881
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-13 00:12:00 -0700

    changed modified API call methods

--- server.js
+++ server.js
@@ -13,59 +13,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
-const runApiCalls = async (queryOptions, initialValue) => {
+app.get('/', (req, res) => {
+  console.log('hello world')
+})
 
-  const response = await fetch(`https://finviz.com/screener.ashx?v=111&f=${queryOptions}&r=${initialValue}`);
-  const html = await response.text();
-  const dom = new JSDOM(html);
+app.get('/search/:initialValue/:queryOptions?', async (req, res) => {
 
-  const htmlElements = Array.from(dom.window.document.getElementsByClassName('screener-link-primary'));
-  const countString = Array.from(dom.window.document.getElementsByClassName('count-text'))[0].textContent.split(' ')[1];
-  const count = parseInt(countString);
+  const { queryOptions, initialValue } = req.params;
 
-  const tickers = htmlElements.map(node => node.textContent).join(',');
-  
-  const jsonData = await fetch(`https://financialmodelingprep.com/api/v3/profile/${tickers}?apikey=${process.env.REACT_APP_FMP_API_KEY}`);
-  return jsonData;
-}
+  const finvizResponse = await fetch(`https://finviz.com/screener.ashx?v=111&f=${queryOptions}&r=${initialValue}`);
+  const finvizHtml = await finvizResponse.text();
+  const finvizDOM = new JSDOM(finvizHtml);
 
-const getCompanyNews = async (ticker) => {
+  const tickers = Array.from(finvizDOM.window.document.getElementsByClassName('screener-link-primary'))
+    .map(node => node.textContent)
+    .join(',');
+
+  let totalResultCount = Array.from(finvizDOM.window.document.getElementsByClassName('count-text'))[0]
+    .textContent
+    .split(' ')[1];
+  totalResultCount = parseInt(totalResultCount);
+
+  const fmpResponse = await fetch(`https://financialmodelingprep.com/api/v3/profile/${tickers}?apikey=${process.env.REACT_APP_FMP_API_KEY}`);
+  const stockData = await fmpResponse.json();
+
+  res.send(JSON.stringify({ stockData, totalResultCount }));
+})
+
+app.get('/companynews/:ticker', async (req, res) => {
   const dateCurr = new Date().toISOString().slice(0, 10);
   let dateOld = new Date();
   dateOld.setMonth(dateOld.getMonth() - 8);
   dateOld = dateOld.toISOString().slice(0, 10);
   const dateQuery = `&from=${dateOld}&to=${dateCurr}`;
 
-  const returnObj = fetch(`https://finnhub.io/api/v1/company-news?symbol=${ticker}${dateQuery}`, {
+  const finnhubData = await fetch(`https://finnhub.io/api/v1/company-news?symbol=${req.params.ticker}${dateQuery}`, {
     method: 'GET',
     headers: { 'X-Finnhub-Token' : process.env.REACT_APP_FINNHUB_API_KEY }
   })
-  .then(jsonData => jsonData.json())
-  .then(articles => {
-    const returnArray = articles.slice(0, 9);
-    returnArray.forEach((article, i) => {
-      article.summary = article.summary.slice(0, 480) + '...';
-    });
-    return  { "newsArray": returnArray }
-  })
-
-  return returnObj;
-}
-
-app.get('/', (req, res) => {
-  console.log('hello world')
-})
-
-app.post('/search', (req, res) => {
-  runApiCalls(req.body.queryOptions, req.body.initialValue)
-  .then(jsonData => jsonData.json())
-  .then(data => { res.send(data) });
-})
-
-app.post('/companynews', (req, res) => {
-  getCompanyNews(req.body.ticker)
-  .then(data => JSON.stringify(data))
-  .then(jsonData => {res.send(jsonData)})
+  const articles = await finnhubData.json();
+  const returnArray = articles.slice(0, 9);
+  returnArray.forEach((article, i) => {
+    article.summary = article.summary.slice(0, 480) + '...';
+  });
+  const returnData = JSON.stringify({ "newsArray": returnArray });
+  res.send(returnData);
 })
 
 app.listen(port, () => {
