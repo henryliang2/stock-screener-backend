c2325d9ad7ae5f36ab98b10903d7b54774083dc1
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-15 14:32:40 -0700

    integrated Passport.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   userId: String,
   displayName: String,
   email: String,
+  image: String,
   stocks: Array
 });
 
--- server.js
+++ server.js
@@ -6,12 +6,49 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const mongoose = require('mongoose');
 const User = require('./models/User.js');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 require('dotenv').config(); 
 
 // initialize express
 
 const app = express();
 const port = 3001;
+
+// Passport Configuration
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+async (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  let user = await User.findOne({userId: profile.id});
+  if(user) done(null, user);
+  else {
+    user = await User.create({
+      userId: profile.id,
+      displayName: profile.displayName,
+      email: profile.emails[0].value,
+      image: profile.photos[0].value,
+      stocks: []
+    })
+    
+    done(null, user);
+  }
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.userId);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findOne({userId: id}, (err, user) => done(err, user));
+});
 
 // MongoDB + Mongoose
 
@@ -25,24 +62,59 @@ db.once('open', function() {
 
 // Middleware
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.REACT_APP_SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 100
+  })
+);
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 
-// Routes
+// Passport Routes
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { 
+    successRedirect: 'http://localhost:3000/',
+    failureRedirect: 'http://localhost:3000/' 
+  }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get("/auth/logout", (req, res) => {
+  req.logout();
+  res.redirect('http://localhost:3000');
+});
+
+// API Routes
 
 app.get('/', (req, res) => {
   console.log('Hello World')
 })
 
-app.get('/sync/:googleId', (req, res) => {
-  User.findOne({userId: req.params.googleId})
-  .then(user => {console.log(user); res.send(user)});
+app.get('/sync', (req, res) => {
+  if(!req.user) { res.send({}); return null };
+
+  User.findOne({userId: req.user.userId})
+  .then(user => { res.send(user) });
 })
 
 app.post('/set', (req, res) => {
   User.findOneAndUpdate(
-    { userId: req.body.googleId },
+    { userId: req.user.userId },
     {$set: { stocks: req.body.stocks }}
   ).then(console.log)
 })
