9ce55c274dafacb638ce28bb7aaa682bdb73e615
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-15 22:01:13 -0700

    deploy test

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get("/auth/logout", (req, res) => {
 // API Routes
 
 app.get('/', (req, res) => {
-  console.log('Hello World')
+  res.send('Hello World');
 })
 
 app.get('/sync', (req, res) => {
