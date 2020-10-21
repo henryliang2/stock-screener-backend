c51b7db0e495de1779b8fcf99f1063c03891de5d
zomgitshenry <henryliang@alumni.ubc.ca>
2020-10-20 18:06:49 -0700

    moved passportconfig into server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const passport = require('passport');
 const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
 require('dotenv').config(); 
-
+const User = require('./models/User.js');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const RouteHandlers = require('./RouteHandlers');
-const PassportConfig = require('./PassportConfig');
 
 // initialize express
 
@@ -18,10 +18,31 @@ const port = process.env.PORT || 8080;
 
 // Passport Configuration
 
-passport.use(PassportConfig.googleStrategy);
+passport.use(new GoogleStrategy({
+  clientID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+async (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  let user = await User.findOne({userId: profile.id});
+  if(user) done(null, user);
+  else {
+    user = await User.create({
+      userId: profile.id,
+      displayName: profile.displayName,
+      email: profile.emails[0].value,
+      image: profile.photos[0].value,
+      stocks: []
+    })
+    
+    done(null, user);
+  }
+}));
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+  console.log(user.userId)
+  done(null, user.userId);
 });
 
 passport.deserializeUser((id, done) => {
